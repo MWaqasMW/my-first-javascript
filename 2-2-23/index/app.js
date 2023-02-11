1eab1a8632f51fde147975dd08d04238508ca56d
MWaqasMW <122200705+MWaqasMW@users.noreply.github.com>
2023-02-11 20:55:14 +0500

    add prompt
    if /else uses

---
+++ 2-2-23/index/app.js
@@ -0,0 +1,18 @@
+
+
+//Chp9
+//Que1
+var city=prompt("Enter city")
+if(city == "karachi"){ 
+    alert("Welcom to city of lights")
+}
+
+//Que2
+var gender = prompt("Enter gender")
+if(gender==male){
+    alert("Good morning Sir")
+}
+else if (gender == female){
+    alert("Good morning Maam")
+}
+else( prompt("piease fill gender"))
